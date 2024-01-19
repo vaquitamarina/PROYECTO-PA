e0c4c529a5dc80ad2de1b66181a06914e2e9e16d
Wilbert Raúl <143809176+wiLLotheRock@users.noreply.github.com>
2024-01-19 10:09:21 -0500

    Update compilador.bat

--- compilador.bat
+++ compilador.bat
@@ -1,4 +1,3 @@
 g++ -c main.cpp -I".\Libraries\SFML-2.6.1\include"
 g++ main.o -o main -L".\Libraries\SFML-2.6.1\lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 .\main.exe
-shutdown /r /t 120
