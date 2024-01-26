0685f576740fbeab1282b15ea155aaec7f30808a
vaquitamarina <cristhianvm55@gmail.com>
2024-01-25 21:19:49 -0500

    Se crearon las piezas faltantes

--- Libraries/ScreenPartida.cpp
+++ Libraries/ScreenPartida.cpp
@@ -43,6 +43,42 @@ clsScreenPartida::clsScreenPartida(RenderWindow *w, clsPartida *p) : clsScreen(w
     piezas[15] = new clsPeon(casillas[7][6],'b');
     piezas[15]->setSprite("./Images/Sprites/peon.png");
 
+    piezas[16] = new clsTorre(casillas[0][0],'n');
+    piezas[16]->setSprite("./Images/Sprites/torre_alt.png");
+    piezas[17] = new clsTorre(casillas[7][0],'n');
+    piezas[17]->setSprite("./Images/Sprites/torre_alt.png");
+    piezas[18] = new clsTorre(casillas[0][7],'b');
+    piezas[18]->setSprite("./Images/Sprites/torre.png");
+    piezas[19] = new clsTorre(casillas[7][7],'b');
+    piezas[19]->setSprite("./Images/Sprites/torre.png");
+    
+    piezas[20] = new clsCaballo(casillas[1][0],'n');
+    piezas[20]->setSprite("./Images/Sprites/caballo_alt.png");
+    piezas[21] = new clsCaballo(casillas[6][0],'n');
+    piezas[21]->setSprite("./Images/Sprites/caballo_alt.png");
+    piezas[22] = new clsCaballo(casillas[1][7],'b');
+    piezas[22]->setSprite("./Images/Sprites/caballo.png");
+    piezas[23] = new clsCaballo(casillas[6][7],'b');
+    piezas[23]->setSprite("./Images/Sprites/caballo.png");  
+    
+    piezas[24] = new clsAlfil(casillas[2][0],'n');
+    piezas[24]->setSprite("./Images/Sprites/alfil_alt.png");
+    piezas[25] = new clsAlfil(casillas[5][0],'n');
+    piezas[25]->setSprite("./Images/Sprites/alfil_alt.png");
+    piezas[26] = new clsAlfil(casillas[2][7],'b');
+    piezas[26]->setSprite("./Images/Sprites/alfil.png");
+    piezas[27] = new clsAlfil(casillas[5][7],'b');
+    piezas[27]->setSprite("./Images/Sprites/alfil.png");
+
+    piezas[28] = new clsReina(casillas[3][0],'n');
+    piezas[28]->setSprite("./Images/Sprites/reina_alt.png");
+    piezas[29] = new clsReina(casillas[3][7],'b');
+    piezas[29]->setSprite("./Images/Sprites/reina.png");
+
+    piezas[30] = new clsRey(casillas[4][0],'n');
+    piezas[30]->setSprite("./Images/Sprites/rey_alt.png");
+    piezas[31] = new clsRey(casillas[4][7],'b');
+    piezas[31]->setSprite("./Images/Sprites/rey.png");
     // partida = p;
     // partida->setPiezas(piezas[0]);
 }
@@ -65,5 +101,23 @@ void clsScreenPartida::draw(){
     window->draw(piezas[13]->getSprite());
     window->draw(piezas[14]->getSprite());
     window->draw(piezas[15]->getSprite());
+    window->draw(piezas[16]->getSprite());
+    window->draw(piezas[17]->getSprite());
+    window->draw(piezas[18]->getSprite());
+    window->draw(piezas[19]->getSprite());
+    window->draw(piezas[20]->getSprite());
+    window->draw(piezas[21]->getSprite());
+    window->draw(piezas[22]->getSprite());
+    window->draw(piezas[23]->getSprite());
+    window->draw(piezas[24]->getSprite());
+    window->draw(piezas[25]->getSprite());
+    window->draw(piezas[26]->getSprite());
+    window->draw(piezas[27]->getSprite());
+    window->draw(piezas[28]->getSprite());
+    window->draw(piezas[29]->getSprite());
+    window->draw(piezas[30]->getSprite());
+    window->draw(piezas[31]->getSprite());
+
+
     // window->draw(partida->getSprite(6,1));
 }
