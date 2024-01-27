a692c5eca9ce2cf66566db32cfba26a49b40e129
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 16:50:17 -0500

    Se añadio logica al caballo

--- Libraries/Caballo.cpp
+++ Libraries/Caballo.cpp
@@ -5,5 +5,29 @@ clsCaballo::clsCaballo(Vector2f pos, char c) : clsPieza(pos,c){
 }
 
 bool clsCaballo::testMovement(Vector2f pos){
+    if(pos.x == posOrigin.x + _CASILLA && pos.y == posOrigin.y + (2 * _CASILLA)){
+        return true;
+    }
+    if(pos.x == posOrigin.x - _CASILLA && pos.y == posOrigin.y - (2 * _CASILLA)){
+        return true;
+    }
+    if(pos.x == posOrigin.x + _CASILLA && pos.y == posOrigin.y - (2 * _CASILLA)){
+        return true;
+    }
+    if(pos.x == posOrigin.x - _CASILLA && pos.y == posOrigin.y + (2 * _CASILLA)){
+        return true;
+    }
+    if(pos.x == posOrigin.x + (2 * _CASILLA) && pos.y == posOrigin.y + _CASILLA){
+        return true;
+    }
+    if(pos.x == posOrigin.x - (2 * _CASILLA) && pos.y == posOrigin.y - _CASILLA){
+        return true;
+    }
+    if(pos.x == posOrigin.x + (2 * _CASILLA) && pos.y == posOrigin.y - _CASILLA){
+        return true;
+    }
+    if(pos.x == posOrigin.x - (2 * _CASILLA) && pos.y == posOrigin.y + _CASILLA){
+        return true;
+    }
     return false;
 }
