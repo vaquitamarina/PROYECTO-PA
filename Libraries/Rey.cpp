96dc9e05f8a099995a4b2df2bb0b6ccfb5b7e809
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 15:26:45 -0500

    Se añadio logica al rey

--- Libraries/Rey.cpp
+++ Libraries/Rey.cpp
@@ -5,5 +5,8 @@ clsRey::clsRey(Vector2f pos, char c) : clsPieza(pos,c){
 }
 
 bool clsRey::testMovement(Vector2f pos){
+    if(pos.x == posOrigin.x + _CASILLA || pos.y == posOrigin.y + _CASILLA || pos.x == posOrigin.x - _CASILLA || pos.y == posOrigin.y - _CASILLA){
+        return true;
+    }
     return false;
 }
--- Libraries/Torre.cpp
+++ Libraries/Torre.cpp
@@ -4,5 +4,8 @@ clsTorre::clsTorre(Vector2f pos, char c) : clsPieza(pos,c){
 }
 
 bool clsTorre::testMovement(Vector2f pos){
+    if(pos.x == posOrigin.x || pos.y == posOrigin.y){
+        return true;
+    }
     return false;
 }
