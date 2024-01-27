58d52a5e7a81b7534a42ec7bdac1599bbaf3f7b1
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 15:37:14 -0500

    Se añadio logica a la reina

--- Libraries/Reina.cpp
+++ Libraries/Reina.cpp
@@ -4,5 +4,28 @@ clsReina::clsReina(Vector2f pos, char c) : clsPieza(pos,c){
 }
 
 bool clsReina::testMovement(Vector2f pos){
+    if(pos.x == posOrigin.x || pos.y == posOrigin.y){
+        return true;
+    }
+    Vector2f posibility;
+    for(int i = 0; i < 7; i ++){
+        posibility = {posOrigin.x + i*_CASILLA,posOrigin.y + i*_CASILLA};
+        if(pos == posibility){
+            return true;
+        }
+        posibility = {posOrigin.x + i*_CASILLA,posOrigin.y - i*_CASILLA};
+        if(pos == posibility){
+            return true;
+        }
+        posibility = {posOrigin.x - i*_CASILLA,posOrigin.y + i*_CASILLA};
+        if(pos == posibility){
+            return true;
+        }
+        posibility = {posOrigin.x - i*_CASILLA,posOrigin.y - i*_CASILLA};
+        if(pos == posibility){
+            return true;
+        }
+    }
+    
     return false;
 }
