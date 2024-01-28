604a4fad3c535814e73ed6c90e2a312a6ab5665b
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 19:09:20 -0500

    Se añadio colision a la torre

--- Libraries/Torre.cpp
+++ Libraries/Torre.cpp
@@ -4,26 +4,17 @@ clsTorre::clsTorre(Vector2f pos, char c) : clsPieza(pos,c){
 }
 
 bool clsTorre::testMovement(Vector2f pos, bool matriz[][8]){
-
-    // if(pos.x == posOrigin.x || pos.y == posOrigin.y){
-    //     for(int i = 0; i < 8; i ++){
-
-    //     }
-    // }
     if(pos == posOrigin){
         return true;
     }
 
     int inicio = posOrigin.y/_CASILLA;
     int final = (pos.y/_CASILLA)-1;
-    cout<<"Inicio : "<< inicio<<endl;
-    cout<<"Final: "<<final<<endl;
 
     if(pos.x == posOrigin.x){
         if(inicio > final){
             for(int i = inicio-2; i > final; i--){
                 if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
-                    cout<<int(posOrigin.x/_CASILLA)-1<<","<<i;
                     return false;
                 }
             }   
@@ -31,7 +22,6 @@ bool clsTorre::testMovement(Vector2f pos, bool matriz[][8]){
         else{
             for(int i = inicio; i <= final; i++){
                 if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
-                    cout<<int(posOrigin.x/_CASILLA)-1<<","<<i;
                     return false;
                 }
             }           
@@ -42,8 +32,6 @@ bool clsTorre::testMovement(Vector2f pos, bool matriz[][8]){
     inicio = posOrigin.x/_CASILLA;
     final = (pos.x/_CASILLA)-1;
 
-    cout<<"Inicio2 : "<< inicio<<endl;
-    cout<<"Final2: "<<final<<endl;
     if(pos.y == posOrigin.y){
         if(inicio > final){
             for(int i = inicio-2; i > final; i--){
