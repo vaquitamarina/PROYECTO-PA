d07cf2ab0d0fbdf84fd3b605124be7782d1a36cf
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 22:43:37 -0500

    Se añadio colision a la reina y alfil

--- Libraries/Reina.cpp
+++ Libraries/Reina.cpp
@@ -7,33 +7,94 @@ bool clsReina::testMovement(Vector2f pos, bool matriz[][8],bool* turno){
     if(pos == posOrigin){
         return true;
     }
-    if(pos.x == posOrigin.x || pos.y == posOrigin.y){
+    
+    int inicio = posOrigin.y/_CASILLA;
+    int final = (pos.y/_CASILLA)-1;
+
+    if(pos.x == posOrigin.x){
+        if(inicio > final){
+            for(int i = inicio-2; i > final; i--){
+                if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
+                    return false;
+                }
+            }   
+        }
+        else{
+            for(int i = inicio; i <= final; i++){
+                if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
+                    return false;
+                }
+            }           
+        }
         *turno = !*turno;
         return true;
     }
+    inicio = posOrigin.x/_CASILLA;
+    final = (pos.x/_CASILLA)-1;
+
+    if(pos.y == posOrigin.y){
+        if(inicio > final){
+            for(int i = inicio-2; i > final; i--){
+                if(matriz[i][int(posOrigin.y/_CASILLA)-1] == true){
+                    return false;
+                }
+            } 
+        }
+        else{
+            for(int i = inicio; i <= final; i++){
+                if(matriz[i][int(posOrigin.y/_CASILLA)-1] == true){
+                    return false;
+                }
+            } 
+        }
+        *turno = !*turno;
+        return true;
+    }
+
+    
     Vector2f posibility;
-    for(int i = 0; i < 7; i ++){
+    for(int i = 0; i < 8; i ++){
         posibility = {posOrigin.x + i*_CASILLA,posOrigin.y + i*_CASILLA};
         if(pos == posibility){
+            for(int i = 1; i < int(pos.y/_CASILLA) - int(posOrigin.y/_CASILLA); i++){
+                if(matriz[int(posOrigin.x/_CASILLA) + i - 1][int(posOrigin.y/_CASILLA) + i - 1] == true){
+                    return false;
+                }
+            }
             *turno = !*turno;
             return true;
         }
         posibility = {posOrigin.x + i*_CASILLA,posOrigin.y - i*_CASILLA};
         if(pos == posibility){
+        
+            for(int i = 1; i < int(posOrigin.y/_CASILLA) - int(pos.y/_CASILLA); i++){
+                if(matriz[int(posOrigin.x/_CASILLA) + i - 1][int(posOrigin.y/_CASILLA) - i - 1] == true){
+                    return false;
+                }
+            }
             *turno = !*turno;
             return true;
         }
         posibility = {posOrigin.x - i*_CASILLA,posOrigin.y + i*_CASILLA};
         if(pos == posibility){
+            for(int i = 1; i < int(pos.y/_CASILLA) - int(posOrigin.y/_CASILLA); i++){
+                if(matriz[int(posOrigin.x/_CASILLA) - i - 1][int(posOrigin.y/_CASILLA) + i - 1] == true){
+                    return false;
+                }
+            }
             *turno = !*turno;
             return true;
         }
         posibility = {posOrigin.x - i*_CASILLA,posOrigin.y - i*_CASILLA};
         if(pos == posibility){
+            for(int i = 1; i < int(posOrigin.y/_CASILLA) - int(pos.y/_CASILLA); i++){
+                if(matriz[int(posOrigin.x/_CASILLA) - i - 1][int(posOrigin.y/_CASILLA) - i - 1] == true){
+                    return false;
+                }
+            }
             *turno = !*turno;
             return true;
         }
     }
-
     return false;
 }
