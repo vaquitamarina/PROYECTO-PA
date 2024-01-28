022aa0b06d9c79a2a82bffae5dcf18e164a90a7c
vaquitamarina <cristhianvm55@gmail.com>
2024-01-27 19:07:52 -0500

    Se añadio colision a la torre

--- Libraries/Torre.cpp
+++ Libraries/Torre.cpp
@@ -13,21 +13,53 @@ bool clsTorre::testMovement(Vector2f pos, bool matriz[][8]){
     if(pos == posOrigin){
         return true;
     }
+
+    int inicio = posOrigin.y/_CASILLA;
+    int final = (pos.y/_CASILLA)-1;
+    cout<<"Inicio : "<< inicio<<endl;
+    cout<<"Final: "<<final<<endl;
+
     if(pos.x == posOrigin.x){
-        for(int i = posOrigin.y/_CASILLA; i <= pos.y/_CASILLA; i = i + _CASILLA){
-            if(matriz[int(posOrigin.x/_CASILLA)][i] == true){
-                return false;
-            }
+        if(inicio > final){
+            for(int i = inicio-2; i > final; i--){
+                if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
+                    cout<<int(posOrigin.x/_CASILLA)-1<<","<<i;
+                    return false;
+                }
+            }   
         }
+        else{
+            for(int i = inicio; i <= final; i++){
+                if(matriz[int(posOrigin.x/_CASILLA)-1][i] == true){
+                    cout<<int(posOrigin.x/_CASILLA)-1<<","<<i;
+                    return false;
+                }
+            }           
+        }
+        
         return true;
     }
+    inicio = posOrigin.x/_CASILLA;
+    final = (pos.x/_CASILLA)-1;
 
+    cout<<"Inicio2 : "<< inicio<<endl;
+    cout<<"Final2: "<<final<<endl;
     if(pos.y == posOrigin.y){
-        for(int i = posOrigin.x/_CASILLA; i <= pos.x/_CASILLA; i = i + _CASILLA){
-            if(matriz[i][int(posOrigin.y/_CASILLA)] == true){
-                return false;
-            }
+        if(inicio > final){
+            for(int i = inicio-2; i > final; i--){
+                if(matriz[i][int(posOrigin.y/_CASILLA)-1] == true){
+                    return false;
+                }
+            } 
         }
+        else{
+            for(int i = inicio; i <= final; i++){
+                if(matriz[i][int(posOrigin.y/_CASILLA)-1] == true){
+                    return false;
+                }
+            } 
+        }
+        
         return true;
     }
     return false;
