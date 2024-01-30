8c08b57e926525cf4da466800bd7479550e0e1c5
Wilbert <143809176+wiLLotheRock@users.noreply.github.com>
2024-01-29 19:20:23 -0500

    arregle el pixel

--- Libraries/Admin.cpp
+++ Libraries/Admin.cpp
@@ -250,7 +250,7 @@ clsAdmin::clsAdmin(RenderWindow *w){
     screen[2]->setTexture(8,"./Images/Config/Barra9.png");
     screen[2]->setTexture(9,"./Images/Config/Barra10.png");
     screen[2]->setPosSprite(1,{200,301});
-    screen[2]->setScaleSprite(1,{8.5,8.5});
+    screen[2]->setScaleSprite(1,{8.4,8.4});
 
    // screen[2]->setSprite(0,"./Images/backgroundConfig.png");
     screen[1]->setSprite(1,"");
